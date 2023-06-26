a118b36aee337c34758080f2aef5dd27d7e282e5
Anmol Singla <99980339+AnmolSingla23@users.noreply.github.com>
2023-06-26 18:51:31 +0530

    Time: 174 ms (31.12%), Space: 71.7 MB (17.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        
+        for(auto i:mp){
+            if(i.second > 1)
+                return true;
+        }
+        return false;
+    }
+};
