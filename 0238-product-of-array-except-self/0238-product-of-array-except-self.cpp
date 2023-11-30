225a887c1f98347d7018800f173c98454ba05b83
Anmol Singla <99980339+AnmolSingla23@users.noreply.github.com>
2023-12-01 01:46:34 +0530

    Time: 15 ms (84.67%), Space: 24.3 MB (72.00%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-     
-        vector<int> arr(nums.size()); 
-        int temp = 1;
-        for(int i = 0;i<nums.size();i++){  //create a vector with product of all elements prev from curr
-            arr[i] = temp;
-            temp = nums[i]*temp;
+        vector<int> temp(nums.size(),1);
+        int product = 1;
+        for(int i=0;i<nums.size();i++){
+            temp[i] = product*temp[i];
+            product = product*nums[i];
         }
         
-        temp = 1;
-        for(int i=arr.size()-1;i>=0;i--){  //then start from last and find the product from right side from 
-            arr[i] = temp*arr[i];          //curr except curr thhis will five us the answer
-            temp = temp*nums[i];
+        product = 1;
+        for(int i=nums.size()-1;i>=0;i--){
+            temp[i] = temp[i]*product;
+            product = product*nums[i];
         }
-        return arr;
+        return temp;
     }
 };
