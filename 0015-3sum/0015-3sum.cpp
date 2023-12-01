aab1ef34b9d07cba9953ef197f0207d67ae51877
Anmol Singla <99980339+AnmolSingla23@users.noreply.github.com>
2023-12-02 00:49:19 +0530

    Time: 122 ms (59.02%), Space: 30.4 MB (38.87%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-     
-        int n = nums.size();
+        
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        vector<vector<int> > ans;
-        for(int i = 0;i<n-2;i++){
-            if(i> 0 && nums[i] == nums[i-1])  //important consition to check duplicates
-                continue;   //will miss it.
-            int j = i+1;
-            int k = n-1;
-            while(j < k){
+        for(int i=0;i<nums.size()-2;i++){
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
-                 ans.push_back({nums[i],nums[j],nums[k]});  
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+                    ans.push_back(temp);
                     j++;
                     k--;
                     while(j < k && nums[j] == nums[j-1])
@@ -23,9 +26,9 @@ public:
                 }
                 else if(sum > 0)
                     k--;
-                else{
+                else
                     j++;
-                }
+
             }
         }
         return ans;
